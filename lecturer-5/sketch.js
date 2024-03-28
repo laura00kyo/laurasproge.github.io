8fddad12e338368f7b9c963c9be1a4dfdfb31d33
laura00kyo <160757494+laura00kyo@users.noreply.github.com>
2024-03-28 11:29:46 +0200

    p5
    new stuff p5js

---
+++ lecturer-5/sketch.js
@@ -0,0 +1,47 @@
+
+let img;
+
+// Load the image and create a p5.Image object.
+function preload() {
+  img = loadImage('./leafphoto.jpg');
+}
+
+function setup() {
+    createCanvas(
+        windowWidth, windowHeight
+        );
+
+}
+function draw() {
+
+    background("cyan");
+    image(img, 0, 0, width, height);
+
+    
+if(
+    
+    mouseX < width / 2
+    &&
+    mouseY < height / 2
+
+
+) {
+
+    circle(mouseX, mouseY, 50);
+}
+
+
+if(
+    
+    mouseX > width / 2
+    &&
+    mouseY > height / 2
+
+
+) {
+
+    square(mouseX, mouseY, 50);
+   
+} 
+
+}
